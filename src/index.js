68c65b5ac6d5b9e7aeee9ce96cde9164fa5c8431
develad <develadb@gmail.com>
2021-01-06 22:55:32 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import App from './App';
 import Beerapp from './Beerapp'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Beerapp />
   </React.StrictMode>,
   document.getElementById('root')
